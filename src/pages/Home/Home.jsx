04c300d42de2b18b7b2759504caec256ddb24015
Andrii <andriy.lavreniuk07@gmail.com>
2024-12-22 17:53:17 +0200

    w

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt='' className='banner-img'/>
+    <div className='hero-caption'>
+      <img src='{hero_title}' alt='' className='caption-img'/>
+      <p>
+      Hundreds of cash-strapped players accept a strange invitation to compete in children's games. Inside, a tempting prize awaits — with deadly high stakes.
+      </p>
+    </div>
+      </div>
     </div>
   )
 }
